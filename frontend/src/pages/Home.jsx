73196fbcbba959e9e62850628d18d62243055498
Ambulanta Care <ambulanta@dr.com>
2026-02-15 16:12:15 +0000

    auto-commit for 980d26bd-5112-4fa5-afee-17cd798d1df6

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
     }
   };
 
-  const serviceIcons = [Users, Heart, Home, Video, ClipboardCheck, FileCheck, FileText];
+  const serviceIcons = [Users, Heart, Home, Video, ClipboardCheck, FileCheck];
   const extraServiceIcons = [Stethoscope, FileText, ClipboardCheck];
 
   return (
